c318e613a2612c74846ddb71e2d293e4c2a0ba31
Jaso85 <83521430+Jaso85@users.noreply.github.com>
2021-05-16 00:03:57 -0300

    Update Global.tsx

--- milkshake-frontend/src/style/Global.tsx
+++ milkshake-frontend/src/style/Global.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from "../milkshake-uikit/src/theme/index.ts"
+import { PancakeTheme } from "../milkshake-uikit/src/theme/index"
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
